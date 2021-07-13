01ff721b74f93bf021eea7351b7994c3fdbe2ea9
wonderlandpark <wonderlandpark@outlook.kr>
2021-07-13 22:45:54 +0900

    chore: changed bot submits count

--- submits.py
+++ submits.py
@@ -6,13 +6,13 @@ current_date = round(datetime.utcnow().timestamp())
 
 
 class Bot:
-    def __init__(self, id: int):
-        self.id = id
+    def __init__(self):
+        self.id = random.randint(1000000000, 9999999999)
         self.date = random.randrange(current_date)
 
 
 # 대기 중인 봇
-submits: List[Bot] = [Bot(random.randrange(i ** 3)) for i in range(100, 1000)]
+submits: List[Bot] = [Bot() for _ in range(1, 10)]
 
 # 승인된 봇
 approved_submits: List[Bot] = []
