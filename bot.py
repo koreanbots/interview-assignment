cb1d64b8b82f3ec6d473d3d94a7d27da38b66832
Mary <mareowst@gmail.com>
2021-07-13 22:58:07 +0900

    chore: made some changes

--- bot.py
+++ bot.py
@@ -5,8 +5,9 @@ import cogs
 
 
 class Bot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix="!", help_command=None)
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix="!", help_command=None, **kwargs)
+        
         cogs.load(self)
 
     async def on_ready(self):
@@ -15,7 +16,9 @@ class Bot(commands.Bot):
     async def on_command_error(self, ctx: commands.Context, exception: Exception):
         if isinstance(exception, commands.CommandNotFound):
             return
+        
         return await ctx.send(f"Error: {exception}")
 
-
-Bot().run(os.environ.get("TOKEN"))
+    
+if __name__ == '__main__':
+    Bot().run(os.environ.get("TOKEN"))
