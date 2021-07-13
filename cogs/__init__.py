098e463f2bd1afb0c4dc3559f42c638ecfb78618
Mary <mareowst@gmail.com>
2021-07-13 22:58:55 +0900

    chore: made some changes

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,5 +1,8 @@
-from discord.ext import commands
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bot import Bot
 
 
-def load(bot: commands.Bot) -> None:
+def load(bot: "Bot") -> None:
     bot.load_extension("cogs.example")
